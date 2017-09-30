b85570e72753ba91e07f1c97f5211c4e7d857241
yingxin <liyx8610@gmail.com>
2017-09-30 10:30:30 -0400

    msg listener

---
+++ shellhack/src/main/java/edu/shellhack/shelder/MessageListener.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package edu.shellhack.shelder;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+/**
+ * @author ThinkPad
+ *
+ */
+public class MessageListener extends ListenerAdapter {
+	
+	public static void main(String[] args)
+            throws LoginException, RateLimitedException, InterruptedException
+    {
+        JDA jda = new JDABuilder(AccountType.BOT).setToken("MzYzNTk4ODM3NDg4NjgwOTYx.DLDkZQ.pQd7kFU3lTKnEwG5UrK50MmIOsg").buildBlocking();
+        jda.addEventListener(new MessageListener());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContent());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContent());
+        }
+    }
+
+}
